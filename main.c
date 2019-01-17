be933cea1817987584875f3944591314b7abeae5
Your Name <you@example.com>
2019-01-17 09:25:23 +0800

    tihuanminganzifu

--- main.c
+++ main.c
@@ -61,6 +61,38 @@ int main()
     }
     if(code == 4)
     {
+        char x;
+        int i = 0;
+        char str[200];
+
+        printf("qingshuruyiduanweizi（yingwen）:\n");
+        scanf("%s", str);
+
+        for (; str[i] != '\0'; i++)
+        {
+
+        char a = str[i];
+        int j = 0;
+        int flag = 0;
+        for (; j < index; j++)
+        {
+        if (a == arr[j])
+            {
+                flag = 1;
+            }
+        }
+
+        if (flag != 0)
+        {
+            str[i] = '*';
+        }
+        }
+
+        printf("%s\n", str);
+
+        printf("dianjihuuichejixu\n");
+        scanf("%c", &x);
+        scanf("%c", &x);
 
     }
      if(code == 5)
