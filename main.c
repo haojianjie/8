56432367998023125354aff68b6777325bdf8f75
Your Name <you@example.com>
2019-01-17 09:08:57 +0800

    kuangjia

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    char arr[100];
+    int index = 0;
+    printf("minganzifuguanlixitong\n");
+
+    while (1)
+    {
+
+        int code;
+
+        printf("1---tianjiaminganci\n");
+        printf("2---shanchuzuihoong\n");
+        printf("3---xianshiquanbuminganci\n");
+        printf("4---tihuanshuruneiringzhongdeminganci\n");
+        printf("5---tuichuxituyigeminganci\n");
+        
+        printf("qingshuruxiangyinggongnengbiaohao\n");
+
+        scanf("%d", &code);
+
+    
+    if(code == 1)
+    {
+        
+    }
+    if(code == 2)
+    {
+        
+    }
+    if(code == 3)
+    {
+        
+    }
+    if(code == 4)
+    {
+
+    }
+     if(code == 5)
+     {
+            printf("dianjihuichetuichuchengxu\n");
+            break;
+    }
+    return 0;
+
+}
+}
